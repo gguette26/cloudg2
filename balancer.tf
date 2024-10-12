4c69feb022c9f8eef6cbcf5998bd241a19cbac53
guillermo <gguete@unicesar.edu.co>
2024-10-12 11:29:09 -0500

    11:28

--- balancer.tf
+++ balancer.tf
@@ -46,7 +46,7 @@ resource "aws_lb_target_group" "web_tg" {
   name     = "web-target-group"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = "aws_vpc.cloud_vpc.id"
+  vpc_id   = aws_vpc.cloud_vpc.id
 
   health_check {
     path                = "/"
